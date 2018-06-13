99ef6b3fb210f56ed681f9b592c338f9f133bb76
Yuwei Zhou <yuwzho@microsoft.com>
2018-06-13 16:14:21 +0800

    cpmpare notification

--- lib/ansible/modules/cloud/azure/azure_rm_autoscale.py
+++ lib/ansible/modules/cloud/azure/azure_rm_autoscale.py
@@ -292,9 +292,9 @@ class AzureRMAutoScale(AzureRMModuleBase):
                 # profile_result_set = set([profile_to_dict(p, self.target) for p in results.profiles or []])
                 # if profile_result_set != set(self.profiles):
                 #     changed = True
-                # notification_result_set = set([notification_to_dict(n) for n in results.notifications or []])
-                # if notification_result_set != set(self.notifications):
-                #     changed = True
+                notification_result_set = set([str(notification_to_dict(n)) for n in results.notifications or []])
+                if notification_result_set != set([str(n) for n in self.notifications or []]):
+                    changed = True
             if changed:
                 # construct the instance will be send to create_or_update api
                 profiles = [AutoscaleProfile(name=p.get('name'),
